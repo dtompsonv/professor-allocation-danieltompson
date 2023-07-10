d28ef471a72af752a7e1c97b7db08d4375da3d66
Daniel Tompson <danieltompsonv@gmail.com>
2023-07-10 20:28:41 -0300

    add project

---
+++ src/main/java/com/project/professor/allocation/ProfessorAllocationDanieltompsonApplication.java
@@ -0,0 +1,13 @@
+package com.project.professor.allocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessorAllocationDanieltompsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessorAllocationDanieltompsonApplication.class, args);
+	}
+
+}
